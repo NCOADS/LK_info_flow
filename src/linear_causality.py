e51619da2833a61e526b0f3a23d04371655a9b7e
Exodus! <49256406+BryanHuang66@users.noreply.github.com>
2025-02-13 10:53:14 +0800

    First Update

---
+++ src/linear_causality.py
@@ -0,0 +1,154 @@
+import numpy 
+from scipy.stats import norm
+
+## TODO: 增加时间滞后项
+## TODO: Panel Data
+
+
+class LinearLKInformationFlow(object):
+    def __init__(self, xp=numpy, dt=1) -> None:
+        """
+        Parameters:
+            xp: Module(numpy or cupy). defualt: numpy.
+            dt: Time gap.
+        """
+        self.xp = xp
+        self.dt = dt
+
+        self.conf_level_99 = norm.ppf(0.995)  # 99% confidence level
+        self.conf_level_95 = norm.ppf(0.975)  # 95% confidence level
+        self.conf_level_90 = norm.ppf(0.95)   # 90% confidence level
+
+    def _split_matrix(self, matrix, segments):
+        slices = [self.xp.s_[start:end] for start, end in segments]
+        row_slices, col_slices = self.xp.meshgrid(slices, slices, indexing="ij")
+        result = self.xp.vectorize(lambda r, c: matrix[r, c], otypes=[object])(row_slices, col_slices)
+        return result
+
+    def _generate_pairs(self,N):
+        '''
+        Generate segements for 1d subspace.
+        '''
+        return [[i, i + 1] for i in range(N)]
+
+    def _cal_diag_inv_cov(self, cov):
+        diag_inv_cov = self.xp.vectorize(lambda x: self.xp.linalg.inv(x), otypes=[object])(self.xp.diagonal(cov))
+        return diag_inv_cov
+
+
+    def _cal_information_flow(self, invC_mul_dC, cov, diag_inv_cov):
+        def cal_block_information_flow_(i, j):
+            return self.xp.trace(invC_mul_dC[i, j] @ cov[j, i] @ diag_inv_cov[i])
+
+        rows, cols = self.xp.indices(invC_mul_dC.shape)
+        information_flow = self.xp.vectorize(cal_block_information_flow_, otypes=[float])(rows, cols)
+        return information_flow
+
+
+    def _cal_dH_noise(self, diag_inv_cov, error_square_mean):
+        def cal_block_dH_noise_(i):
+            return self.xp.trace(error_square_mean[i,i] @ diag_inv_cov[i])
+
+        rows = self.xp.arange(error_square_mean.shape[0])
+        dH_noise = self.xp.vectorize(cal_block_dH_noise_, otypes=[float])(rows) *1/2
+        return dH_noise
+    
+    def _cal_information_flow_std(self, invC_mul_dC, cov, inv_cov, diag_inv_cov, error_square_mean, n):
+        def cal_block_cal_variance_(i, j):
+            temp = cov[i,j].T@diag_inv_cov[i]
+            variance = self.xp.trace(invC_mul_dC[i, j].T @ diag_inv_cov[i] @ invC_mul_dC[i, j] @(cov[j,j] - cov[j,i]@diag_inv_cov[i]@cov[i,j]))/n\
+                        + self.xp.trace((temp.T@inv_cov[j,j]@temp@error_square_mean[i,i]))
+            return variance
+        
+        rows, cols = self.xp.indices(invC_mul_dC.shape)
+        information_flow_variance = self.xp.vectorize(cal_block_cal_variance_, otypes=[float])(rows, cols)
+        return self.xp.sqrt(information_flow_variance)
+
+    # def _cal_information_flow_std_old_version(self, invC_mul_dC, cov, inv_cov, diag_inv_cov, error_square_mean, n):
+    #     def cal_block_cal_variance_(i, j):
+    #         temp = cov[i,j].T@diag_inv_cov[i]
+    #         variance = self.xp.trace((temp.T@inv_cov[j,j]@temp@error_square_mean[i,i]))
+    #         return variance
+        
+    #     rows, cols = self.xp.indices(invC_mul_dC.shape)
+    #     information_flow_variance = self.xp.vectorize(cal_block_cal_variance_, otypes=[float])(rows, cols)
+    #     return self.xp.sqrt(information_flow_variance)
+
+
+    def causality_estimate(self, ts_data, segments = None, significance_test = True):
+        """
+        Calculate Liang-Kleeman information flow under linear conditions with significance test.
+
+        Parameters:
+            ts_data: Time series(length of time series, number of variables).
+            segments: A list defining the row and column intervals for dividing the matrix, e.g., [[0, 5], [5, 10]],[[0,1],[1,2],[2,3]].
+
+        """
+        self.significance_test = significance_test
+        ts_length,ts_var_num = ts_data.shape
+        assert ts_length>ts_var_num , f"Assertion failed: length of time series ({ts_length}) must be greater than the number of variables ({ts_var_num})."
+        if segments==None:
+            segments = self._generate_pairs(ts_var_num)
+
+        delta_ts_data = (ts_data[1:,:] - ts_data[:ts_length - 1, :]) / (self.dt)
+        ts_data_process = ts_data[:ts_length - 1, :]
+
+
+        x_centered = ts_data_process - self.xp.mean(ts_data_process, axis=0) 
+        cov = x_centered.T @ x_centered 
+        if significance_test:
+            inv_cov = self.xp.linalg.inv(cov)
+            inv_cov = self._split_matrix(inv_cov, segments)
+
+        invC_mul_dC, _, _, _ = self.xp.linalg.lstsq(x_centered, delta_ts_data, rcond=None) ## TODO: further develop for significance test for subspace causality
+        
+        error_vec = delta_ts_data - x_centered@invC_mul_dC
+        error_square_mean = error_vec.T@error_vec/(ts_length-ts_var_num -1)
+        self.error_vec = error_vec
+        self.error_square_mean = error_square_mean
+
+        segments = [sorted(item) for item in segments]
+        cov = self._split_matrix(cov, segments)
+        invC_mul_dC = self._split_matrix(invC_mul_dC.T, segments)
+        error_square_mean = self._split_matrix(error_square_mean, segments)
+        diag_inv_cov = self._cal_diag_inv_cov(cov)
+
+
+        ## calculate informtaion flow
+        information_flow = self._cal_information_flow(invC_mul_dC, cov, diag_inv_cov)
+        self.information_flow = information_flow
+
+        ## calculate normalized information flow
+        dH_noise = self._cal_dH_noise(diag_inv_cov, error_square_mean)
+        self.dH_noise = dH_noise
+        normalizer =  self.xp.sum(self.xp.abs(information_flow), axis=1) + self.xp.abs(dH_noise)
+        normalized_information_flow = information_flow/normalizer
+        self.normalized_information_flow = normalized_information_flow
+
+        if significance_test:
+            information_flow_std = self._cal_information_flow_std(invC_mul_dC, cov, inv_cov, diag_inv_cov, error_square_mean, (ts_length-ts_var_num-1))
+            self.information_flow_std = information_flow_std
+            # information_flow_std_old_version = self._cal_information_flow_std_old_version(invC_mul_dC, cov, inv_cov, diag_inv_cov, error_square_mean, (ts_length-ts_var_num-1))
+            # self.information_flow_std_old_version = information_flow_std_old_version
+            self.p = (1 - norm.cdf(self.xp.abs(self.information_flow / self.information_flow_std))) * 2 # p-value
+
+        
+    def get_dict(self):
+        if hasattr(self, 'information_flow'):
+            state_dict = {
+            "information_flow": self.information_flow,
+            "normalized_information_flow": self.normalized_information_flow,
+            }
+            if self.significance_test:
+                state_dict.update({
+                    "information_flow_std": self.information_flow_std,
+                    "statistics":{
+                        "p99_critical_value": self.information_flow_std*self.conf_level_99,
+                        "p95_critical_value": self.information_flow_std*self.conf_level_95,
+                        "p90_critical_value": self.information_flow_std*self.conf_level_90,
+                        "p": self.p
+                        }
+                })
+            return state_dict
+        else:
+            return "Causality estimate has not been run yet. Please run 'causality_estimate' first!"
